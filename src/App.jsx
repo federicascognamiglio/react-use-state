99955bd63ef7691716c25a723224a77e4142863c
federicascognamiglio <scognamigliofede99@gmail.com>
2024-12-16 16:03:30 +0100

    feat: buttons created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
+import languages from './data/languages'
 
 function App() {
 
   return (
     <>
-    
+      <div className="container">
+        <header className='pt-4'>
+          <h1>Learn Web Development</h1>
+        </header>
+        <main className="main">
+          <section className='pt-4'>
+            {languages.map(curLanguage => <button key={curLanguage.id} className='btn btn-primary me-3'>{curLanguage.title}</button>)}
+          </section>
+        </main>
+      </div>
     </>
   )
 }
